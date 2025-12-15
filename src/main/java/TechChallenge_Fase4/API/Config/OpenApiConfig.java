bef22ffc2ea60b4c5209ad5afe5cd2c731ac7e62
Julia Souza <juliasouza1429@gmail.com>
2025-12-14 22:22:58 -0300

    add swagger config,

---
+++ src/main/java/TechChallenge_Fase4/API/Config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package TechChallenge_Fase4.API.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tech Challenge API")
+                        .version("v1")
+                        .description("Documentação da API Spring Boot"));
+    }
+}
